4bbfb1e177e7ca41cf60b780c6f3716fb61110d6
Jennifer Cole <capauldi@gmail.com>
2022-03-06 02:19:53 -0800

    route indexes connections

---
+++ routes/index.js
@@ -0,0 +1,10 @@
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+router.use((req, res) => {
+    res.status(404).send('404 Error: Page not found');
+});
+
+module.exports = router;
